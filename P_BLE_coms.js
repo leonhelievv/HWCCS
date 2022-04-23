04babaa1bfc03edd221f023f22727dd6538a158f
leonhelievv <83067322+leonhelievv@users.noreply.github.com>
2022-04-23 16:29:16 +0200

    Add files via upload

--- P_BLE_coms.js
+++ P_BLE_coms.js
@@ -46,21 +46,25 @@ async function findHWCS_original() {
  async function findHWCS() {
  	theEvent = event
  	debug1.innerHTML = '---------- at findHWCS ------'
- 	device = await navigator.bluetooth.requestDevice({
-        acceptAllDevices: true
-     });
-     window.HWCCS_Server = await device.gatt.connect(service_uuid);
-     service = await  HWCCS_Server.getPrimaryService(service_uuid);
-     characteristic = await service.getCharacteristic(characteristic_uuid_RX);
-     console.log('---------- at findHWCS ------');
-     console.log('the requested device name is ' + device.name);
-     debug1.innerHTML = 'the requested device name is ' + device.name
-     //connect to the HWCCS
-     //BLE_setup()
-     let HWCCSConnected = await connectDeviceAndCacheCharacteristic(device);
-     console.log('is the HWCCS connected ? '+HWCCSConnected)
-     debug1.innerHTML = 'is the HWCCS connected ? '+HWCCSConnected
-     //connected do cleanup
-     cleanupBLEComs()
+ 	device = await navigator.bluetooth.requestDevice({acceptAllDevices: true });
+ 	debug1.innerHTML = '------await navigator.bluetooth.requestDevice-----'
+   window.HWCCS_Server = await device.gatt.connect(service_uuid);
+   debug1.innerHTML = '-------await device.gatt.connect------'
+   service = await  HWCCS_Server.getPrimaryService(service_uuid);
+   debug1.innerHTML = '----------await  HWCCS_Server.getPrimaryService------'
+   characteristic = await service.getCharacteristic(characteristic_uuid_RX);
+   debug1.innerHTML = '---------- await service.getCharacteristic ------'
+ 
+   console.log('the requested device name is ' + device.name);
+   debug2.innerHTML = 'the requested device name is ' + device.name
+   
+   //connect to the HWCCS
+   //BLE_setup()
+   let HWCCSConnected = await connectDeviceAndCacheCharacteristic(device);
+   
+   console.log('is the HWCCS connected ? '+HWCCSConnected)
+   debug1.innerHTML = 'connected to  '+HWCCSConnected
+   //connected do cleanup
+   cleanupBLEComs()
  }
 
